72ed678dccdd9c39859bed4bff12456115303376
usermokhtar <mokhtar.soumair.idrissi@gmail.com>
2024-08-01 13:36:06 +0100

    first edit of Header and CoreConcept Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import Header from './Components/Header';
+import Header from './Components/Header/Header';
+import CoreConcept from './Components/CoreConcept';
+import { CORE_CONCEPTS } from './data.js';
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((item) => (
+              <CoreConcept key={item.title} {...item} />
+            ))}
+          </ul>
+        </section>
       </main>
     </div>
   );
