3e62bc3bd4cd714d3231879951bf86b51123f2b5
usermokhtar <mokhtar.soumair.idrissi@gmail.com>
2024-08-01 01:53:22 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './Components/Header';
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Img1 from '../assets/react-core-concepts.png';
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+const Header = () => {
+  let startDescription = reactDescriptions[genRandomInt(2)];
+  return (
+    <div>
+      <header>
+        <img src={Img1} alt='Stylized atom' />
+        <h1>React Essentials</h1>
+        <p>
+          {startDescription} React concepts you will need for almost any app you
+          are going to build!
+        </p>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
